805d01dee83ba11be58f97e1dcd0f649055d8ab8
Zhang Xianqiang <qiangai_001@163.com>
2016-02-19 18:09:37 +0800

    f
    fff

--- Borther/SecondSpider.py
+++ Borther/SecondSpider.py
@@ -18,12 +18,25 @@ print('Second Spider')
 #     jikespider = spider()
 #     all_links = jikespider.changepage()
 
+# 豆瓣悬疑电影
+url = 'http://movie.douban.com/typerank?type_name=%E6%82%AC%E7%96%91&type=10&interval_id=100:90&action='
+# url = 'http://www.qiushibaike.com'
+headers = {'User-Agent': 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25'}
+# 添加一个 headers
+html = requests.get(url, headers=headers)
+# html2 = requests.get(url)
+print(html.text)
 
+sr = r'<span class="movie-name-text">.*?</span>'
 
+# sr = r'<span class="movie-name-text"> <a herf=".*?">(.*?)</a>'
 
+moiveName = re.findall(sr, html.text, re.S)
 
+print('moiveName list', moiveName)
 
-
+for each in moiveName:
+    print(each)
 
 
 
